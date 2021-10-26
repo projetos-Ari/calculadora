11b72ecbf1a0536dd240cec1de204b0e3467ffc1
Ariane Teruel <80228375+projetos-Ari@users.noreply.github.com>
2021-10-26 09:52:26 -0300

    Delete variaveis directory

--- variaveis/exemplo.js
+++
@@ -1,9 +0,0 @@
-var texto = "Vamos fazer uma calculadora";
-var num1 = 25;
-var num2 = 26;
-soma = num1+num2
-alert(soma);
-//document.write(texto)
-console.log(num1);
-
-document.getElementById('teste').innerHTML = texto;
