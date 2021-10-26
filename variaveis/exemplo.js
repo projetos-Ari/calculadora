69e10d73cd2aa2b219bb0dd16f0c28e03143489e
Ariane Teruel <80228375+projetos-Ari@users.noreply.github.com>
2021-10-26 09:51:59 -0300

    Add files via upload

---
+++ variaveis/exemplo.js
@@ -0,0 +1,9 @@
+var texto = "Vamos fazer uma calculadora";
+var num1 = 25;
+var num2 = 26;
+soma = num1+num2
+alert(soma);
+//document.write(texto)
+console.log(num1);
+
+document.getElementById('teste').innerHTML = texto;
